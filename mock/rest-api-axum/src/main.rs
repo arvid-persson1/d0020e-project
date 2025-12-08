597b18a8f5eddce9ba27697204be4804943a8e12
johanR04 <johan.s@risberg.com>
2025-12-08 15:26:44 +0100

    Done some formatting with cargo fmt

--- mock/rest-api-axum/src/main.rs
+++ mock/rest-api-axum/src/main.rs
@@ -3,32 +3,32 @@ mod handlers;
 use std::sync::{Arc, Mutex};
 
 use axum::{
-  Router,
-  routing::{get, post},
+    Router,
+    routing::{get, post},
 };
 
-use handlers::{APPState, get_books, get_book, add_book};
+use handlers::{APPState, add_book, get_book, get_books};
 
 #[tokio::main]
-async fn main(){
+async fn main() {
+    let books = vec![];
 
-  let books = vec![];
+    let state = Arc::new(APPState {
+        books: Arc::new(Mutex::new(books)),
+    });
 
-  let state = Arc::new(APPState{
-    books: Arc::new(Mutex::new(books)),
-  });
+    //Create router for axum
+    let app = Router::new()
+        .route("/books", get(get_books))
+        .route("/books", get(get_book))
+        .route("/books", post(add_book))
+        .with_state(state);
 
-  //Create router for axum
-  let app = Router::new()
-    .route("/books", get(get_books))
-    .route("/books", get(get_book))
-    .route("/books", post(add_book)).with_state(state);
+    //Define listener for axum (TCP: IP and port)
+    let addrs = "127.0.0.1:1616";
+    println!("App is listening on {}", addrs);
+    let tcplisn = tokio::net::TcpListener::bind(addrs).await.unwrap();
 
-  //Define listener for axum (TCP: IP and port)
-  let addrs = "127.0.0.1:1616";
-  println!("App is listening on {}", addrs);
-  let tcplisn = tokio::net::TcpListener::bind(addrs).await.unwrap();
-
-  //Call axum serve to start the web server
-  axum::serve(tcplisn, app).await.unwrap();
+    //Call axum serve to start the web server
+    axum::serve(tcplisn, app).await.unwrap();
 }
