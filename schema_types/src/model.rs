0fd1fb4881e1a0f3c0db0e94bc05cbe9d45f7ea7
arvid-persson1 <a.persson04@gmail.com>
2025-12-15 00:36:58 +0100

    renamed fields in model, allowed multiple types for properties

--- schema_types/src/model.rs
+++ schema_types/src/model.rs
@@ -2,10 +2,10 @@ use std::hash::{Hash, Hasher};
 
 #[derive(Clone, Debug)]
 pub struct SchemaClass {
-    label: String,
-    // NOTE: Only labels should be stored, makes instantiation easier.
+    name: String,
+    // NOTE: Only names should be stored, makes instantiation easier.
     subclass_of: Box<[String]>,
-    comment: String,
+    description: String,
     // NOTE: Should only include those defined on the class directly, not on any of its supertypes.
     // These will be added during instantiation.
     properties: Box<[Property]>,
@@ -16,10 +16,11 @@ pub struct SchemaClass {
 // such, this does not reference the class it belongs to.
 #[derive(Clone, Debug)]
 pub struct Property {
-    label: String,
-    comment: String,
-    range: String,
-    // A tuple (class label, property label).
+    name: String,
+    description: String,
+    // WARN: Must not be empty.
+    expected_types: Box<[String]>,
+    // A tuple (class name, property name).
     inverse_of: Option<(String, String)>,
     // Superseded items should be marked as deprecated and link to their successor.
     superseded_by: Option<String>,
@@ -30,16 +31,16 @@ impl PartialEq for SchemaClass {
     #[inline]
     fn eq(&self, other: &Self) -> bool {
         let Self {
-            label,
+            name,
             subclass_of,
-            comment,
+            description: comment,
             properties,
         } = self;
-        *label == other.label && {
-            // Labels should uniquely identify classes. Hence, if the labels are equal, the classes
+        *name == other.name && {
+            // Labels should uniquely identify classes. Hence, if the names are equal, the classes
             // should be entirely equal.
             debug_assert!(*subclass_of == other.subclass_of);
-            debug_assert!(*comment == other.comment);
+            debug_assert!(*comment == other.description);
             debug_assert!(*properties == other.properties);
             true
         }
@@ -51,7 +52,7 @@ impl Eq for SchemaClass {}
 impl Hash for SchemaClass {
     #[inline]
     fn hash<H: Hasher>(&self, state: &mut H) {
-        self.label.hash(state);
+        self.name.hash(state);
     }
 }
 
@@ -59,17 +60,17 @@ impl PartialEq for Property {
     #[inline]
     fn eq(&self, other: &Self) -> bool {
         let Self {
-            label,
-            comment,
-            range,
+            name,
+            description: comment,
+            expected_types: range,
             inverse_of,
             superseded_by,
         } = self;
-        *label == other.label && {
-            // Labels should uniquely identify properties. Hence, if the labels are equal, the
+        *name == other.name && {
+            // Labels should uniquely identify properties. Hence, if the names are equal, the
             // properties should be entirely equal.
-            debug_assert!(*comment == other.comment);
-            debug_assert!(*range == other.range);
+            debug_assert!(*comment == other.description);
+            debug_assert!(*range == other.expected_types);
             debug_assert!(*inverse_of == other.inverse_of);
             debug_assert!(*superseded_by == other.superseded_by);
             true
@@ -82,6 +83,6 @@ impl Eq for Property {}
 impl Hash for Property {
     #[inline]
     fn hash<H: Hasher>(&self, state: &mut H) {
-        self.label.hash(state);
+        self.name.hash(state);
     }
 }
