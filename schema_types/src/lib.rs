c5a42f31582f2855ada138c1fe842a53f7fa02d8
arvid-persson1 <a.persson04@gmail.com>
2025-12-14 22:39:35 +0100

    fixed unfulfilled clippy lint

--- schema_types/src/lib.rs
+++ schema_types/src/lib.rs
@@ -1,6 +1,6 @@
 #![expect(missing_docs)]
-#![expect(clippy::missing_docs_in_private_items)]
 #![expect(clippy::allow_attributes_without_reason)]
+#![allow(clippy::missing_docs_in_private_items)]
 
 pub mod example;
 pub mod model;
