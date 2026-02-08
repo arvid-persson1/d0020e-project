0c7ddaae56d750c35cdad87eea0f0439e5b5e3f8
johanR04 <johan.s@risberg.com>
2026-02-08 14:12:32 +0100

    sql_connector (sql api): Implemented some more tests of the sql api

--- mock/sql_api/src/api_testing.rs
+++ mock/sql_api/src/api_testing.rs
@@ -34,8 +34,9 @@ fn init_test_db() -> db::DbPool {
 #[tokio::test]
 async fn books_test() {
     let pool = init_test_db();
-    let app = builder::build_app(pool);
+    let app = builder::build_app(pool.clone());
 
+    // Test 1: test of get_books_list
     let response_list = app
         .clone()
         .oneshot(
@@ -65,6 +66,7 @@ async fn books_test() {
     assert_eq!(books[0].get_format(), models::BookFormatType::Hardcover);
     assert_eq!(books[0].get_isbn(), "9780316497541");
 
+    // Test 2: Test of get_book
     let response_single = app
         .clone()
         .oneshot(
@@ -89,4 +91,49 @@ async fn books_test() {
     assert_eq!(book.get_title(), "The Last Wish: Introducing the Witcher");
     assert_eq!(book.get_author(), "Andrzej Sapkowski");
     assert_eq!(book.get_format(), models::BookFormatType::Hardcover);
+
+    // Test 3: Not found test
+    let response404 = app
+        .clone()
+        .oneshot(
+            Request::builder()
+          .uri("/books/0000000000000") // This ISBN does not exist
+          .body(Body::empty())
+          .expect("Failed to build request"),
+        )
+        .await
+        .expect("Request failed");
+
+    assert_eq!(response404.status(), StatusCode::NOT_FOUND);
+
+    // Manually clears the database contents.
+    {
+        let mut conn = pool.get().expect("Failed to get connection");
+        let _unused = diesel::delete(schema::books::table)
+            .execute(&mut conn)
+            .expect("Failed to clear table");
+    };
+
+    // Requests the list
+    let response = app
+        .clone()
+        .oneshot(
+            Request::builder()
+                .uri("/books")
+                .body(Body::empty())
+                .expect("Failed to build request"),
+        )
+        .await
+        .expect("Request failed");
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body_bytes = to_bytes(response.into_body(), usize::MAX)
+        .await
+        .expect("Failed to read body");
+
+    let empty_books: Vec<models::Book> =
+        serde_json::from_slice(&body_bytes).expect("Failed to deserialize");
+
+    assert!(empty_books.is_empty(), "Expected empty list, found books!");
 }
