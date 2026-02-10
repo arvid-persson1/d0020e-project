ed1531165591a7d27319b5a096b650839cc7182e
arvid-persson1 <a.persson04@gmail.com>
2026-02-10 15:10:51 +0100

    updated demo code

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -5,30 +5,38 @@ use broker::{
     Broker,
     connector::Source,
     encode::json::Json,
-    query::Queryable,
+    query::{
+        Queryable,
+        combinators::{And, Or},
+    },
     rest::{Build as _, Builder as RestBuilder},
 };
 use serde::Deserialize;
+use std::fmt::{Display, Error as FmtError, Formatter};
 use tokio::main;
 
 #[derive(Deserialize, Debug, Queryable)]
 struct Book {
     title: String,
     author: String,
-    format: BookFormatType,
     isbn: String,
 }
 
-#[derive(Deserialize, Debug)]
-enum BookFormatType {
-    Epub,
-    Hardcover,
-    Paperback,
+impl Display for Book {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), FmtError> {
+        let Self {
+            title,
+            author,
+            isbn,
+        } = self;
+        write!(f, "\"{title}\" by {author}; ISBN {isbn}")
+    }
 }
 
 #[main]
 async fn main() {
     let mut broker = Broker::<Book>::new();
+    println!("Instantiated broker.");
 
     broker.add_source(Box::new(
         RestBuilder::new()
@@ -37,9 +45,48 @@ async fn main() {
             .decoder(Json)
             .build(),
     ));
+    println!("Registered source (REST endpoint on 127.0.0.1:8080).");
+
+    println!();
 
     let query = Book::author().eq("Jane Austen");
-    println!("query: {query:#?}");
+    println!("Defined query: {query:#?}\n");
+
     let results = broker.fetch_all(&query).await;
-    println!("results: {results:#?}");
+    match results {
+        Ok(v) if v.is_empty() => {
+            println!("No books matching query.");
+        },
+        Ok(v) => {
+            for book in v {
+                println!("{book}");
+            }
+        },
+        Err(_) => {
+            println!("An error occurred.");
+            return;
+        },
+    }
+
+    let query = And(
+        Book::author().eq("George Orwell"),
+        Or(Book::title().eq("1984"), Book::title().eq("Animal Farm")),
+    );
+    println!("Defined query: {query:#?} [1 residue]\n");
+
+    let results = broker.fetch_all(&query).await;
+    match results {
+        Ok(v) if v.is_empty() => {
+            println!("No books matching query.");
+        },
+        Ok(v) => {
+            for book in v {
+                println!("{book}");
+            }
+        },
+        Err(_) => {
+            println!("An error occurred.");
+            return;
+        },
+    }
 }
